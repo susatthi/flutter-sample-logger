326aa28157d3ac6ef29132cce849c0078c3f04f8
susa.keyber <susa@keyber.jp>
2022-04-13 15:52:53 +0900

    print

---
+++ lib/main_print.dart
@@ -0,0 +1,12 @@
+void main() {
+  // ログレベル
+  print('Hello Print!');
+
+  // 型
+  print(1000);
+  print(true);
+  print([1, 2, 3]);
+  print({'key': 'key', 'value': 'value'});
+  print(Exception('例外もいけます'));
+  print(() => '関数はいけません');
+}
